66d13e563d9baa68afc61f127fb84bfb66e4e6a0
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2018-12-10 02:01:38 -0500

    Typedefs

--- include/typedefs.h
+++ include/typedefs.h
@@ -5,11 +5,18 @@
 #ifndef FEATHER_TYPEDEFS_H
 #define FEATHER_TYPEDEFS_H
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
 typedef std::string DOMString;
-
-std::vector<DOMString> parseStringList(DOMString list);
+typedef std::int_fast8_t Byte;
+typedef std::int_fast16_t Short;
+typedef std::int_fast32_t Int;
+typedef std::int_fast64_t Long;
+typedef std::uint_fast8_t UByte;
+typedef std::uint_fast16_t UShort;
+typedef std::uint_fast32_t UInt;
+typedef std::uint_fast64_t ULong;
 
 #endif //FEATHER_TYPEDEFS_H
