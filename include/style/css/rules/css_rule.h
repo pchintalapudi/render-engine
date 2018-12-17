98fc65b11f2a7123e19c07b0add1298bf624dc1e
Justin Ley <inkysigma@outlook.com>
2018-12-17 01:37:50 -0800

    Attempting to do a commit before cleaning up build system.

---
+++ include/style/css/rules/css_rule.h
@@ -0,0 +1,16 @@
+//
+// Created by binhe on 12/17/2018.
+//
+
+#ifndef FEATHER_CSS_RULE_H
+#define FEATHER_CSS_RULE_H
+
+namespace feather {
+    namespace css {
+        namespace rules {
+
+        }
+    }
+}
+
+#endif //FEATHER_CSS_RULE_H
